50aa8ae6cb6fa083cd5b1ceea046803f18e83119
angrykun <2545662859@qq.com>
2017-11-02 17:04:02 +0800

    string新增NET2.0环境

---
+++ CLRViaCSharp/FourteenthNet2.0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FourteenthNet2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s11 = "abc";
+            Console.WriteLine(string.IsInterned(s11) ?? "null");
+            string s22 = "a";
+            Console.WriteLine(string.Intern(s22) ?? "null");
+        }
+    }
+}
