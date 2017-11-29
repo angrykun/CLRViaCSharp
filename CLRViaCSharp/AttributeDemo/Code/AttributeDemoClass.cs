a4f618f01faf736fb98cb03ce07a2b86b14f35a1
angrykun <2545662859@qq.com>
2017-11-29 17:50:00 +0800

    第十八章 定制attribute

---
+++ CLRViaCSharp/AttributeDemo/Code/AttributeDemoClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributeDemo.Code
+{
+    /*
+     * 第十八章 定制attribute
+     * 
+     * **/
+    public class AttributeDemoClass
+    {
+    }
+}
