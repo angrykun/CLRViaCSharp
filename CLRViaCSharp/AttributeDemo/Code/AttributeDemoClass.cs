6377a83bd38bedaafa28c7bec9bf1c8603dde563
angrykun <2545662859@qq.com>
2017-11-29 18:05:47 +0800

    attribute

--- CLRViaCSharp/AttributeDemo/Code/AttributeDemoClass.cs
+++ CLRViaCSharp/AttributeDemo/Code/AttributeDemoClass.cs
@@ -9,6 +9,10 @@ namespace AttributeDemo.Code
     /*
      * 第十八章 定制attribute
      * 
+     * ①利用attribute可以声明性地为自己的代码构造添加注解。
+     *  关于自定义attribute：它只是将一些附加信息与某个目标元素
+     *  关联起来的方式。
+     * 
      * **/
     public class AttributeDemoClass
     {
